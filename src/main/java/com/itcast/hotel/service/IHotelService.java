70bee80fc327059db725f93fd5859aecf50fcab7
AlickLiubc <nrlbc@sina.com>
2023-07-09 01:05:57 +0800

    first commit

---
+++ src/main/java/com/itcast/hotel/service/IHotelService.java
@@ -0,0 +1,7 @@
+package com.itcast.hotel.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itcast.hotel.pojo.Hotel;
+
+public interface IHotelService extends IService<Hotel> {
+}
---
+++ src/test/java/com/itcast/hotel/HotelIndexTest.java
@@ -0,0 +1,48 @@
+package com.itcast.hotel;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static com.itcast.hotel.constants.HotelConstants.MAPPING_TEMPLATE;
+
+public class HotelIndexTest {
+    private RestHighLevelClient client;
+
+    @Test
+    void testInit() {
+        System.out.println(client);
+    }
+
+    @Test
+    void testCreateHotelIndex() throws IOException {
+        // 1.创建Request对象
+        CreateIndexRequest request = new CreateIndexRequest("hotel");
+
+        // 2.请求参数
+        request.source(MAPPING_TEMPLATE, XContentType.JSON);
+
+        // 3.发起请求
+        client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.client = new RestHighLevelClient(RestClient.builder(
+                HttpHost.create("http://127.0.0.1:9200")
+        ));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        this.client.close();
+    }
+}
