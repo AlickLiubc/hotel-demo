d5659b3f4563ae3446b0ec60cea00a915985ea25
AlickLiubc <nrlbc@sina.com>
2023-07-09 16:56:25 +0800

    新增删除索引库与查询索引库是否存在的单元测试方法

--- src/test/java/com/itcast/hotel/HotelIndexTest.java
+++ src/test/java/com/itcast/hotel/HotelIndexTest.java
@@ -1,10 +1,12 @@
 package com.itcast.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,6 +34,27 @@ public class HotelIndexTest {
 
         // 3.发起请求
         client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteHotelIndex() throws IOException {
+        // 1.创建Request对象
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("hotel");
+
+        // 2.发起请求
+        client.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testExistsHotelIndex() throws IOException {
+        // 1.创建Request对象
+        GetIndexRequest getIndexRequest = new GetIndexRequest("hotel");
+
+        // 2.发起请求
+        boolean exists = client.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
+
+        // 3.输出
+        System.err.println(exists ? "索引库已存在！" : "索引库不存在！");
     }
 
     @BeforeEach
