26028a7e90d93c97e6883a8c7182f1a85abd23a4
AlickLiubc <nrlbc@sina.com>
2023-07-31 23:20:06 +0800

    修改配置类信息

--- src/main/java/cn/itcast/hotel/config/MqConfig.java
+++ src/main/java/cn/itcast/hotel/config/MqConfig.java
@@ -12,17 +12,17 @@ import org.springframework.context.annotation.Configuration;
 public class MqConfig {
     @Bean
     public TopicExchange topicExchange() {
-        return new TopicExchange(MqConstants.HOTEL_EXCHANGE);
+        return new TopicExchange(MqConstants.HOTEL_EXCHANGE, true, false);
     }
 
     @Bean
     public Queue insertQueue() {
-        return new Queue(MqConstants.HOTEL_INSERT_QUEUE);
+        return new Queue(MqConstants.HOTEL_INSERT_QUEUE, true);
     }
 
     @Bean
     public Queue deleteQueue() {
-        return new Queue(MqConstants.HOTEL_DELETE_QUEUE);
+        return new Queue(MqConstants.HOTEL_DELETE_QUEUE, true);
     }
 
     @Bean
